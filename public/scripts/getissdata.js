88bc8991009e0558eb4394d704d37ad864dc7589
Victoria Aoka <victoria.aoka@andela.com>
2018-10-25 19:32:52 +0300

    Revert "Feature use google maps to display iss location #161210531"

--- public/scripts/getissdata.js
+++
@@ -1,9 +0,0 @@
-//Get ISS data from Open Notify and pass it to the url.
-let button = document.getElementsByClassName('showbtn')[0];
-  button.addEventListener('click', () => {
-    fetch(`http://api.open-notify.org/iss-now.json`).then(response => {
-        return response.json();
-      }).then(data => {
-        window.location.href = `/map/?lat=${data.iss_position.latitude}&long=${data.iss_position.longitude}`;
-      }).catch(err => {console.log(err)});
-  });
