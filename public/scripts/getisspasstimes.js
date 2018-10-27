19d630305eed0ab1f85973d2cbf8a52c03ed50ef
victoriaaoka <victoria.aoka@andela.com>
2018-10-27 16:53:55 +0300

    [Feature #161437395] Add code to delay the visibility of page contents till after geodata has been received

--- public/scripts/getisspasstimes.js
+++ public/scripts/getisspasstimes.js
@@ -1,9 +1,15 @@
 //Get passtimes from Open Notify and display them in a table.
 const placeData = document.getElementsByClassName('data');
-const table = document.getElementsByClassName("passtimestable")[0];
+const table = document.getElementsByClassName("passtimestable")[0]; 
+const passTimesExtras = document.getElementsByClassName('pastimesextras')[0];
+const searchTextBox = document.getElementById('srch');
 
 //Use JSONP data to get the next 5 Iss pass-times and add them to a table. The timezone is the default OS timezone.
 $.getJSON(`http://api.open-notify.org/iss-pass.json?lat=${placeData[1].innerHTML}&lon=${placeData[2].innerHTML}&n=5&callback=?`, function(data) {
+    const loader = document.getElementById("loader");
+    loader.innerHTML = `The table below shows the next 5 ISS pass times for ${placeData[3].innerHTML} <i>[ ${placeData[0].innerHTML} ]</i>`
+    table.style.visibility = 'visible';
+    passTimesExtras.style.visibility = 'visible';
     data['response'].forEach(function (d) {
         const date = new Date(d['risetime']*1000);
         const duration = d['duration']
@@ -14,4 +20,3 @@ $.getJSON(`http://api.open-notify.org/iss-pass.json?lat=${placeData[1].innerHTML
         durationCell.innerHTML = duration;
     });
 });
-    
